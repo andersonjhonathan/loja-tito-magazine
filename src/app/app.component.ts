957517926f09a2d2f8ac138bcc3cc8306e1b7477
Anderson J <andersonjhonathan@gmail.com>
2025-04-01 19:36:36 -0300

    implementação de novas telas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent, BenefitsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
