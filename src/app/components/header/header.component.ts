f2a63761c68bbea4a43e4185cf467b0c2f8b1437
Anderson J <andersonjhonathan@gmail.com>
2025-04-15 23:38:53 -0300

    implementaçãologin + botao dashboard

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginPopoverComponent } from './modal-login/login-popover.component';
+import { AuthService } from './modal-login/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, LoginPopoverComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  @ViewChild('loginBtn', { static: false }) loginButton!: ElementRef;
+  isAdmin: boolean = false;
+  mostrarPopover = false;
+  popoverPosition = { top: '0px', left: '0px' };
+  
+  constructor(private authService: AuthService) {}
+  private authSubscription: Subscription | null = null;
 
+
+  ngOnInit() {
+    // Inscrever-se para mudanças no estado de admin
+    this.authSubscription = this.authService.isAdmin$.subscribe(isAdmin => {
+      this.isAdmin = isAdmin;
+    });
+  }
+
+  ngOnDestroy() {
+    // Limpar a assinatura ao destruir o componente
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+  
+  togglePopover() {
+    this.mostrarPopover = !this.mostrarPopover;
+  
+    if (this.mostrarPopover && this.loginButton?.nativeElement?.getBoundingClientRect) {
+      const rect = this.loginButton.nativeElement.getBoundingClientRect();
+      this.popoverPosition = {
+        top: `${rect.bottom + window.scrollY + 10}px`,
+        left: `${rect.left + window.scrollX}px`
+      };
+    }
+  }
+  
 }
